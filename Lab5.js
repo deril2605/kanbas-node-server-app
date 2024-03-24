6f9638ea5b13efcfc4ccc95503e4ccb7471f5270
deril2605 <derilraju@gmail.com>
2024-03-24 23:11:35 +0530

    axios and cors

--- Lab5.js
+++ Lab5.js
@@ -130,7 +130,7 @@ const Lab5 = (app) => {
     });
 
     app.get("/a5/welcome", (req, res) => {
-        res.send("Welcome to Assignment 5");
+        res.send("Welcome to Assignment 5!!!");
     });
 
     app.get("/a5/add/:a/:b", (req, res) => {
