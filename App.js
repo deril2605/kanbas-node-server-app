cd2d0ebac9e40f56c6e80ba1bb6a624724441579
deril2605 <derilraju@gmail.com>
2024-03-26 14:27:14 -0400

    fix folder struc 2

--- App.js
+++ App.js
@@ -2,9 +2,9 @@ import express from 'express';
 import Hello from "./Hello.js"
 import Lab5 from './Lab5.js';
 import cors from "cors";
-import CourseRoutes from "./src/Kanbas/courses/routes.js";
-import ModuleRoutes from "./src/Kanbas/Modules/routes.js";
-import AssignmentRoutes from './src/Kanbas/assignments/routes.js';
+import CourseRoutes from "./Kanbas/courses/routes.js";
+import ModuleRoutes from "./Kanbas/Modules/routes.js.js";
+import AssignmentRoutes from './Kanbas/assignments/routes.js';
 
 const app = express()
 app.use(cors());
